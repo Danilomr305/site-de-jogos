25f802ebde6b0da200d8d2b0ba088a9fafb546e3
Danilomr305 <danilomr305@gmail.com>
2023-06-22 12:57:36 -0300

    .

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const search = document.querySelector('.search');
 const icon = document.querySelector('.icon');
 
 icon.onclick = function() {
-    search.classList.fixed('active'); 
+    search.classList.forEach('active'); 
 }
 
 // function 2
