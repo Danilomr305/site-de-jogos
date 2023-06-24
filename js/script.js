a76ce8e7d36dc6eba58b87a08319b88c10229c81
Danilomr305 <danilomr305@gmail.com>
2023-06-24 19:45:45 -0300

    .

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ icon.onclick = function() {
     search.classList.toggle('active')
 }
 
-// function 2
+// -----------
+const url = 'https://poki.com.br/g/subway-surfers?campaign=14345571062&adgroup=129035160471&extensionid=&targetid=aud-1966070259771:dsa-1408254915829&location=9101562&matchtype=&network=g&device=c&devicemodel=&creative=604135175923&keyword=&placement=&target=&gclid=Cj0KCQjwqNqkBhDlARIsAFaxvwyA4uU8rVUXMD-gUB2L-4dqbRPopIMp2K7gmmSwx3iDFIyrRjC5c7gaAhhIEALw_wcB';
+const sub = document.querySelector('#sub');
+
+function openInNewTab(url) {
+    const win = window.open(url, 'blank');
+    win.focus();
+}
+
+sub.addEventListener('click', () => {
+    openInNewTab(url)
+})
